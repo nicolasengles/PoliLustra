5e7d4f043066d69a01e8f248ba73b9307950f91d
Nicolas Engles <nicolasmengles@gmail.com>
2025-10-05 23:14:16 -0300

    SASS removido; Docker removido; Navbar atualizada; Página inicial criada; Livereload com Cross-env adicionado;

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
-// 1. IMPORTAÇÕES E CONFIGURAÇÃO INICIAL
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// 2. MIDDLEWARE
+if (process.env.NODE_ENV == 'development') {
+  const livereload = require('livereload');
+  const connectLiveReload = require('connect-livereload');
+  
+  const liveReloadServer = livereload.createServer();
+  liveReloadServer.watch(path.join(__dirname, 'public'));
+  
+  app.use(connectLiveReload());
+}
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'html')));
 app.use('/styles', express.static(path.join(__dirname, 'styles')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
 
-// 3. ROTA PRINCIPAL (OPCIONAL, MAS BOA PRÁTICA)
-// Esta rota garante que ao aceder a "/", o seu index.html é servido.
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'html', 'index.html'));
 });
 
-// 4. INICIAR O SERVIDOR
 app.listen(port, () => {
-    console.log(`Servidor a rodar em http://localhost:${port}`);
+    console.log(`Servidor rodando em http://localhost:${port} (${process.env.NODE_ENV})`);
 });
