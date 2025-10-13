1fb20635de0cd6dbc4897e82f3d6456469c5f771
Nicolas Engles <nicolasmengles@gmail.com>
2025-10-13 11:45:01 -0300

    Arquivos HTML convertidos em views EJS; Páginas login e register feitas responsivas; head, navbar e footer transformados em templates EJS;

--- server.js
+++ server.js
@@ -4,24 +4,33 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-if (process.env.NODE_ENV == 'development') {
-  const livereload = require('livereload');
-  const connectLiveReload = require('connect-livereload');
-  
-  const liveReloadServer = livereload.createServer();
-  liveReloadServer.watch(path.join(__dirname, 'public'));
-  
-  app.use(connectLiveReload());
-}
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'html')));
+
 app.use('/styles', express.static(path.join(__dirname, 'styles')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'html', 'index.html'));
+    res.render('index');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.get('/configuracoes', (req, res) => {
+  res.render('configuracoes');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
 });
 
 app.listen(port, () => {
